3e3196f693736c0f201e76c0bbfa23d485d9e280
Juan I. Bartamian <juan.i.bartamian@gmail.com>
2021-07-13 20:38:05 -0300

    examen terminado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Codificaciones />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Codificaciones from './components/Codificaciones.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Codificaciones,
   }
 }
 </script>
---
+++ src/components/Codificaciones.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="src-codificaciones">
+      <div class="jumbotron">
+        <h2>Exámen Final - Juan Ignacio Bartamian</h2>
+        <br>
+        <input type="text" v-model="texto">
+        <hr>
+        <h5> cantidad: {{ cantidadCaracteres}}</h5>
+        <h5>{{ texto | replace("a","e")}} (codificado)</h5>
+        <h5>{{ texto | mayus}} (Mayúscula)</h5>
+        <h5>{{ texto | minus}} (Minúscula)</h5>
+        <h5>{{ texto | mayusMinus}} (Mayúscula/Minúscula)</h5>
+        <h5>{{ texto | minusMayus}} (Minúscula/Mayúscula)</h5>
+      </div>
+      <p>respuestas:1:b 2:b 3:c 4:b 5:b </p>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-codificaciones',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        texto: "",
+      }
+    },
+    filters: {
+      minusMayus: function(texto) {
+        return texto.split('').map((char,index) => index % 2 == 0 ? char.toLowerCase() : char.toUpperCase()).join(''); 
+      },
+      mayusMinus: function(texto) {
+        return texto.split('').map((char,index) => index % 2 == 0 ? char.toUpperCase() : char.toLowerCase()).join(''); 
+      },
+      mayus: function(texto) {
+        return texto.toUpperCase();
+      },
+      minus: function(texto) {
+        return texto.toLowerCase();
+      },
+      codificado: function(texto) {
+        return texto.replace('o','e')
+      },
+      replace: function (texto) {
+      const result = texto.toLowerCase().split("o").join("e").split("u").join("a");
+      return result;
+      }
+    },
+    methods: {
+    },
+    computed: {
+      cantidadCaracteres(){
+        return this.texto.length
+      }
+      
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-codificaciones {
+
+  }
+
+</style>
